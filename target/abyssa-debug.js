7af95ad602179634b3cb9eb8260477b30c24f525
Alex Galays <bagonzago@yahoo.fr>
2013-10-08 21:53:46 +0200

    Make anchor click interception work on IE8... Phew. Thanks to sompylasar for spotting the compatibility issue.

--- target/abyssa-debug.js
+++ target/abyssa-debug.js
@@ -3700,26 +3700,46 @@ Router.enableLogs = function() {
 Abyssa.Router = Router;
 
 function interceptAnchorClicks(router) {
-  document.addEventListener('click', function(evt) {
-    if (evt.defaultPrevented || evt.metaKey || evt.ctrlKey || evt.button == 1) return;
-
-    var anchor = anchorTarget(evt.target);
-
-    if (!anchor) return;
-    if (anchor.getAttribute('target') == '_blank') return;
-    if (anchor.hostname != location.hostname) return;
-
-    evt.preventDefault();
-    router.state(anchor.getAttribute('href'));
-  });
+  if (document.addEventListener)
+    document.addEventListener('click', anchorClickHandler);
+  else
+    document.attachEvent('onclick', anchorClickHandler);
 }
 
+function anchorClickHandler(evt) {
+  evt = evt || window.event;
+  if (evt.defaultPrevented || evt.metaKey || evt.ctrlKey || !isLeftButtonClick(evt)) return;
+
+  var anchor = anchorTarget(evt.target);
+
+  if (!anchor) return;
+  if (anchor.getAttribute('target') == '_blank') return;
+  if (getHostname(anchor) != location.hostname) return;
+
+  evt.preventDefault();
+  router.state(anchor.getAttribute('href'));
+}
+
+function isLeftButtonClick(evt) {
+  evt = evt || window.event;
+  var button = (evt.which !== undefined) ? evt.which : evt.button;
+  return button == 1;
+}
 
 function anchorTarget(target) {
   while (target) {
     if (target.nodeName == 'A') return target;
     target = target.parentNode;
   }
+}
+
+function getHostname(anchor) {
+  if (anchor.hostname) return anchor.hostname;
+
+  // IE can lose the hostname property when setting a relative href from JS.
+  var tempAnchor = document.createElement("a");
+  tempAnchor.href = anchor.href;
+  return tempAnchor.hostname;
 }
 
 this.Abyssa = Abyssa;
