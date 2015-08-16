c2a701678ca079cb0a5a75097567718e8b48e1c5
Alexandre Galays <agl@zenexity.com>
2015-08-16 09:55:56 +0200

    Build addon and store it in the repo so it can be used in JsFiddle, etc.

---
+++ target/ReactState.js
@@ -0,0 +1,64 @@
+
+/* Addon making using React easier */
+
+'use strict';
+
+(function () {
+
+  var React = typeof require == 'function' ? require('react') : window.React;
+
+  function ReactStateForContainer(container) {
+    return function ReactState(uri, component, children) {
+      var state = {
+        _component: component,
+        uri: uri,
+        children: children
+      };
+
+      // The component class chain.
+      var components;
+
+      if (children) {
+        var names = Object.keys(children);
+
+        if (!names.some(function (name) {
+          return children[name].uri == '';
+        })) children._default_ = ReactState('');
+
+        names.forEach(function (name) {
+          return children[name]._parent = state;
+        });
+      }
+
+      state.enter = function (params) {
+        // It is the responsability of the leaf state to render the whole component chain; Bail if we're a parent.
+        if (children) return;
+
+        if (!components) {
+          var parents = [];
+          var parent = state._parent;
+
+          while (parent) {
+            parents.push(parent._component);
+            parent = parent._parent;
+          }
+          parents.reverse();
+          components = component ? parents.concat(component) : parents;
+        }
+
+        var instance;
+        if (components.length == 1) instance = React.createElement(components[0], { params: params });else instance = components.reduceRight(function (child, parent) {
+          if (!React.isValidElement(child)) child = React.createElement(child, { params: params });
+
+          return React.createElement(parent, { params: params }, child);
+        });
+
+        React.render(instance, container);
+      };
+
+      return state;
+    };
+  }
+
+  typeof module == 'object' ? module.exports = ReactStateForContainer : Abyssa.ReactState = ReactStateForContainer;
+})();
